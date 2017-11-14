c797e480a6a0f5038e426a8cd6911e0ed8ed4e52
canky <can_ky@hotmail.com>
2017-11-14 17:27:48 +0100

    - TEST

--- craft/config/elementapi.php
+++ craft/config/elementapi.php
@@ -31,7 +31,7 @@ return [
         ],
         'program.json' => [
             'elementType' => ElementType::Entry,
-            'cache' => false,
+            'cache' => true,
             'criteria' => ['section' => 'program','type' =>'institution'],
             'transformer' => function(EntryModel $entry) {
 
